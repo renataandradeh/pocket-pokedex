091d02413f61dce4bffda888c3ce57a52c072feb
Renata Andrade <renataandradeh@MacBook-Pro-de-Renata-2.local>
2020-09-11 16:39:04 -0300

    Initial setup.

---
+++ Pokedex_Renata/Pokedex_Renata/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  Pokedex_Renata
+//
+//  Created by Renata Andrade on 11/09/20.
+//  Copyright © 2020 Renata Gondim Andrade. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Pokedex_Renata/Pokedex_Renata/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex_Renata
+//
+//  Created by Renata Andrade on 11/09/20.
+//  Copyright © 2020 Renata Gondim Andrade. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
